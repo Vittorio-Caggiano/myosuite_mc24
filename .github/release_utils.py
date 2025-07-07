d4dac931fa6a85650d6f9ac885e8ef6bf13dfce6
Vittorio Caggiano <caggiano@gmail.com>
2025-07-07 15:15:16 -0400

    Update release_utils.py

--- .github/release_utils.py
+++ .github/release_utils.py
@@ -42,14 +42,12 @@ def update_version(new_version_tuple) -> None:
 
     with open("myosuite/version.py", "r") as reader:
         current_version_data = reader.read()
-
-    # for line in current_version_data:
     version_match = re.search(
-        r"^__version_tuple__ ", current_version_data, re.MULTILINE
+        r"^__version_tuple__ = \(.*\)", current_version_data, re.MULTILINE
     )
 
     if version_match:
-        new_version_data = "__version_tuple__ = %s\n" % str(new_version_tuple)
+        new_version_data = "__version_tuple__ = %s" % str(new_version_tuple)
         current_version_data = current_version_data.replace(
             version_match.group(), new_version_data
         )
